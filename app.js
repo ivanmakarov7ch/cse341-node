510baacddeaa8b9bb8a6b16beb65a86449e81e34
ivanmakarov7ch <ivanmak.7ch@gmail.com>
2025-09-06 16:42:02 +0300

    Changet port from 3000 to 8080

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 const mongodb = require('./db/database');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 
 app.use(express.static(path.join(__dirname, 'frontend')));
