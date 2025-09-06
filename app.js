bb7e879f4b2f94c378d21c99f34941484fd9fab5
ivanmakarov7ch <ivanmak.7ch@gmail.com>
2025-09-06 10:15:40 +0300

    Added Frontend folder to API

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
- 
-app.get('/', (req, res) => {
+const path = require('path'); //for frontend folder
+
+app.use(express.static(path.join(__dirname, 'frontend'))); //for frontend folder
+
+app.get('/greeting', (req, res) => {
   res.send("Hello Ivan into Node.JS!");
 });
  
+
+//my test of implenet front end
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
+});
+
+
 app.listen(process.env.PORT || 3000, () => {
   console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
 });
