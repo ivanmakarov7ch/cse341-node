ef8c0ad8137fbf63a73d2e4a73a31dd5cc53fb18
ivanmakarov7ch <ivanmak.7ch@gmail.com>
2025-09-12 19:57:39 +0300

    reverting the code

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,25 @@
 import js from "@eslint/js";
 import globals from "globals";
 import json from "@eslint/json";
-import { defineConfig } from "eslint/config";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
-  { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended"] },
-  {ignores: ["package-lock.json", "node_modules/", "dist/", "build/", ".env"],},
-]);
+  {
+    files: ["**/*.json"],
+    plugins: { json },
+    language: "json/json",
+    extends: ["json/recommended"],
+  },
+  {
+    files: ["**/*.jsonc"],
+    plugins: { json },
+    language: "json/jsonc",
+    extends: ["json/recommended"],
+  },
+];
